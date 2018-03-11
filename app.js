9af0a16f1e6186d802489d19c345b1d0722d9f66
Yugaank Sharma <yugaanks@yahoo.co.in>
2018-03-11 16:05:05 -0400

    port value is not forced

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.set('view engine', 'handlebars');
 
 configRoutes(app);
 
-app.listen(process.env.PORT || 5000, '0.0.0.0', () => {
+app.listen(process.env.PORT || 5000, () => {
     console.log("We've now got a server!");
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
